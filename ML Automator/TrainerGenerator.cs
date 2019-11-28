a683c1ab3c6ddcfbbb95cd71e6dcaf08b692e46f
James Kellie <Jpk_GameID@hotmail.com>
2019-11-28 21:24:25 +1100

    Update TrainerGenerator.cs
    - Restructured public variables
    - Added more comments
    - Renamed several variables
    - Removed unused usings

--- ML Automator/TrainerGenerator.cs
+++ ML Automator/TrainerGenerator.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
 
 namespace ML_Automator
 {
     class TrainerGenerator
     {
-        private readonly ResearchTracker researchTracker = null;
-
-        private readonly int trainingSteps = 10;
-
+        /// <summary> Added to the beggining of Anaconda Arguments as it simplifies summaries</summary>
+        public string CurrentRunID { get; private set; } = string.Empty;
+        /// <summary> How many times are needed to train before moving up to the next level of training. </summary>
         public int SessionsPerStep { get; } = int.MinValue;
+        /// <summary> Returns the path to the Trainer that Anaconda has to use to run. </summary>
+        public string TrainerFullPath
+        {
+            get
+            {
+                return $"{runtimeTrainerPath}{runtimeTrainerName}";
+            }
+        }
+
+        private readonly ResearchTracker researchTracker = null;
+        // Settings that are loaded from config.json
+        private readonly int trainingSteps = 10;
+        private readonly bool skipSameTrainers = true;
 
         private const string trainingDefaultsPath = "./Resources/TrainingDefaults.json";
         private readonly Dictionary<string, string> trainingDefaultConfig = null;
@@ -22,32 +31,31 @@ namespace ML_Automator
         private const string trainingMaxPath = "./Resources/TrainingMax.json";
         private readonly Dictionary<string, string> trainingMaxConfig = null;
 
-        /// <summary> Dictionary of the Iterative values so that we don't repeatedly generate the same values for each session. </summary>
+        /// <summary> Dictionary of the Iterative values that are used when generating new trainers, reducing the need for repeated calculations. </summary>
         private readonly Dictionary<string, float> trainningIterationValues = null;
 
-        private const string defaultYamlTrainerPath = "./Resources/DefaultTrainer.yaml";
-        private readonly string defaultYamlString = string.Empty;
+        private const string regexYamlTrainerPath = "./Resources/DefaultTrainer.yaml";
+        private readonly string regexYamlString = string.Empty;
 
-        private readonly string modifiedTrainerPath = "./Resources/";
-        private readonly string modifiedTrainerName = "trainer.yaml";
+        private readonly string runtimeTrainerPath = "./Resources/";
+        private readonly string runtimeTrainerName = "trainer.yaml";
 
         /// <summary> Used to compare previous to newly generated Yaml, Int changes can result in duplicate trainning, and we can skip these to save some trainning time. </summary>
         private string previousModifiedYaml = string.Empty;
         /// <summary> The current Yaml used for trainning. </summary>
         private string activeModifiedYaml = string.Empty;
-        /// <summary> Added to the beggining of Anaconda Arguments as it simplifies summaries</summary>
-        public string CurrentRunID { get; private set; } = string.Empty;
 
-        public TrainerGenerator(ResearchTracker tracker, int steps)
+        public TrainerGenerator(ResearchTracker tracker, int steps, bool skipSameTrainer = true)
         {
             researchTracker = tracker;
             trainingSteps = steps;
+            skipSameTrainers = skipSameTrainer;
             Util.PrintConsoleMessage(ConsoleColor.Green, $"TrainerGenerator using {steps} step{ (steps > 1 ? "s" : string.Empty)} for training");
             // Load our JSON files into dictionaries so that we can manipulate these when generating new Trainning Data.
             Util.LoadJsonFromLocation(trainingDefaultsPath, ref trainingDefaultConfig);
             Util.LoadJsonFromLocation(trainingMaxPath, ref trainingMaxConfig);
             // Read contents of file to our Yaml.
-            defaultYamlString = Util.ReadFile(defaultYamlTrainerPath);
+            regexYamlString = Util.ReadFile(regexYamlTrainerPath);
             // How many times we train at the current 'base' value with the combination of configurations
             SessionsPerStep = trainingMaxConfig.Count + 1;
 
@@ -64,12 +72,9 @@ namespace ML_Automator
             previousModifiedYaml = activeModifiedYaml;
             Util.PrintConsoleMessage(ConsoleColor.DarkGreen, $"Generating New Training Data");
             // Copy our Default Trainer
-            activeModifiedYaml = defaultYamlString;
+            activeModifiedYaml = regexYamlString;
             // MultiStep is used to determine Which combination of configuration settings to use for current training
             int BaseMultiplier = (Math.DivRem(Step, trainingMaxConfig.Count + 1, out int MultiStep) % trainingSteps) ;
-            /// I've kept the below incase I return to use binary step if I refine my variables, 10 is just to omany as it means 1000 sessions of trainning which is insane.
-            //x // We convert our MultiStep to a Binary representation so that we can check against the chars for if we want to adjust the value from the Multiplier
-            //x string BinaryStep = Convert.ToString(MultiStep, 2).PadLeft(trainingMaxConfig.Count, '0');
             // This allows us to check the index against our Binary Configuration quick
             int ConfigIndex = 0;
             foreach (var item in trainingMaxConfig.Keys)
@@ -92,7 +97,7 @@ namespace ML_Automator
                 activeModifiedYaml = activeModifiedYaml.Replace($"<{item}>", trainingDefaultConfig[item]);
             }
             // If Prev and Active are the Same we return false, which will skip the current trainning.
-            if (previousModifiedYaml == activeModifiedYaml)
+            if (skipSameTrainers == true && previousModifiedYaml == activeModifiedYaml)
             {
                 return false;
             }
@@ -103,14 +108,18 @@ namespace ML_Automator
                 {
                     if (MultiStep == 0)
                     {
-                        // This means we've moved up to the next BaseMultiplier or next 'level' of trainning, we ask our Logger to prepare us ranks for all the previous sessions before we start a new log.
-                        researchTracker.RankStepSessions();
+                        // We don't want to rank stuff if we only have 1 file, so we skip ranks in those cases
+                        if (trainningIterationValues != null && trainningIterationValues.Count > 0)
+                        {
+                            // This means we've moved up to the next BaseMultiplier or next 'level' of trainning, we ask our Logger to prepare us ranks for all the previous sessions before we start a new log.
+                            researchTracker.RankStepSessions();
+                        }
                     }
                     Util.MoveFolderContents(new DirectoryInfo("./summaries"), new DirectoryInfo($"{researchTracker.CurrentResearchStepFolder}/summaries"));
                     Util.MoveFolderContents(new DirectoryInfo("./models"), new DirectoryInfo($"{researchTracker.CurrenResearchLogPath}/models"));
                 }
                 // Otherwise we save the Modified Yaml
-                File.WriteAllText(modifiedTrainerPath + modifiedTrainerName, activeModifiedYaml);
+                File.WriteAllText(runtimeTrainerPath + runtimeTrainerName, activeModifiedYaml);
                 // Log actions
                 Util.PrintConsoleMessage(ConsoleColor.Green, $"-------------------------------------");
                 Util.PrintConsoleMessage(ConsoleColor.Green, $"Starting Train Loop {BaseMultiplier}\t|\t STEP: {MultiStep}/{SessionsPerStep}");
