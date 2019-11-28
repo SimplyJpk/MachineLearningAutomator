5eabbfe7073fd17f46c6b40d1c414bb72b320030
James Kellie <Jpk_GameID@hotmail.com>
2019-11-28 21:31:42 +1100

    Update AnacondaLoad.bat
    Removed the trainer path from the bat which will now be passed in during runtime to allow the config file to change where this file is located.

--- ML Automator/Resources/AnacondaLoad.bat
+++ ML Automator/Resources/AnacondaLoad.bat
@@ -2,4 +2,4 @@ ECHO Launching Anaconda
 REM Activate our Conda Environment
 CALL conda activate L:/Anaconda/envs/ml-agents
 REM Launch our Learning
-CALL mlagents-learn ./Resources/trainer.yaml %*
+CALL mlagents-learn %*
